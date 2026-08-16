e9497e7545b885f877aa8e733090f8aeafc5d267
Harshking001 <ekey6343@gmail.com>
2026-08-16 12:28:23 +0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Navbar = () => {
   return (
     <nav>
-      <div>
+      <div className="navbar">
+        <img src="/Drinks/images/fav.png" alt="fresh"/>
+        <button>Explore</button>
       </div>
     </nav>
   )
